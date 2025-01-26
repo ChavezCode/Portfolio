5c5720ca2a789f8856e86a353af8d2d907b1f883
ChavezCode <jch1248@wgu.edu>
2025-01-25 20:02:01 -0800

    Work on form submission

--- submit_form.php
+++ submit_form.php
@@ -1,21 +1,49 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require "C:\Program Files\PHP\PHPMailer\PHPMailer-master\src\Exception.php";
+require "C:\Program Files\PHP\PHPMailer\PHPMailer-master\src\PHPMailer.php";
+require "C:\Program Files\PHP\PHPMailer\PHPMailer-master\src\SMTP.php";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
+    $fname = htmlspecialchars($_POST['fname']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $to = 'jchavezflores368@gmail.com'; // Replace with your email address
-    $subject = 'Contact Form Submission from ' . $name;
-    $headers = "From: " . $email . "\r\n" .
-               "Reply-To: " . $email . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
-    
-    $email_body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $mail = new PHPMailer(true);
 
-    if (mail($to, $subject, $email_body, $headers)) {
-        echo "Thank you for your message. We will get back to you soon.";
-    } else {
-        echo "There was a problem sending your message. Please try again.";
+    try {
+        // Set PHPMailer to use SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Use Gmail's SMTP server
+        $mail->SMTPAuth = true;
+        $mail->Username = 'jchavezflores368@gmail.com'; // Your Gmail address
+        $mail->Password = 'B00rito5';  // Your Gmail password or App Password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        // Sender and recipient info
+        $mail->setFrom($email, $fname);  // Sender's email and name
+        $mail->addAddress('jchavezflores368@gmail.com', 'Jorge');  // where the email is going to
+
+        // Set email format to HTML
+        $mail->isHTML(true);
+
+        // Set email subject and body
+        $mail->Subject = 'Contact Form Submission from ' . $fname;
+        $mail->Body    = "Name: $fname<br>
+                        Email: $email<br><br>
+                        Message:<br>$message";
+
+        // Send the email
+        if ($mail->send()) {
+            echo "Thank you for your message. I'll get back to you soon!";
+        } else {
+            echo "Ohh no! There was a problem sending your message. Please try again.";
+        }
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
-?>
